3ce13fba0bd80dafe512817dacef352ae9a08961
wuzhe666 <luckstarwz@163.com>
2019-05-06 18:51:36 +0800

    1

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,7 +1,6 @@
 class TestLogin:
 
     def test_login1(self):
-        print("test_login1")
         assert 1
 
     def test_login2(self):
@@ -10,7 +9,7 @@ class TestLogin:
 
     def test_login3(self):
         print("test_login3")
-        assert 1
+        assert 0
 
     def test_login4(self):
         print("test_login4")
